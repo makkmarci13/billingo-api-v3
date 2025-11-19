4e782db1e7e500b995fa67eefbc10586cbcf7d80
Makai Marcell <hello@makaimarcell.com>
2025-11-19 14:55:22 +0100

    [Add] Stripe billing method

--- lib/Model/PaymentMethod.php
+++ lib/Model/PaymentMethod.php
@@ -27,6 +27,7 @@
  */
 
 namespace Swagger\Client\Model;
+
 use \Swagger\Client\ObjectSerializer;
 
 /**
@@ -43,33 +44,35 @@ class PaymentMethod
      * Possible values of this enum
      */
     const ARUHITEL = 'aruhitel';
-const BANKCARD = 'bankcard';
-const BARION = 'barion';
-const BARTER = 'barter';
-const CASH = 'cash';
-const CASH_ON_DELIVERY = 'cash_on_delivery';
-const COUPON = 'coupon';
-const ELORE_UTALAS = 'elore_utalas';
-const EP_KARTYA = 'ep_kartya';
-const KOMPENZACIO = 'kompenzacio';
-const LEVONAS = 'levonas';
-const ONLINE_BANKCARD = 'online_bankcard';
-const OTHER = 'other';
-const PAYLIKE = 'paylike';
-const PAYONEER = 'payoneer';
-const PAYPAL = 'paypal';
-const PAYPAL_UTOLAG = 'paypal_utolag';
-const PAYU = 'payu';
-const PICK_PACK_PONT = 'pick_pack_pont';
-const POSTAI_CSEKK = 'postai_csekk';
-const POSTAUTALVANY = 'postautalvany';
-const SKRILL = 'skrill';
-const SZEP_CARD = 'szep_card';
-const TRANSFERWISE = 'transferwise';
-const UPWORK = 'upwork';
-const UTALVANY = 'utalvany';
-const VALTO = 'valto';
-const WIRE_TRANSFER = 'wire_transfer';
+    const BANKCARD = 'bankcard';
+    const BARION = 'barion';
+    const BARTER = 'barter';
+    const CASH = 'cash';
+    const CASH_ON_DELIVERY = 'cash_on_delivery';
+    const COUPON = 'coupon';
+    const ELORE_UTALAS = 'elore_utalas';
+    const EP_KARTYA = 'ep_kartya';
+    const KOMPENZACIO = 'kompenzacio';
+    const LEVONAS = 'levonas';
+    const ONLINE_BANKCARD = 'online_bankcard';
+    const OTHER = 'other';
+    const PAYLIKE = 'paylike';
+    const PAYONEER = 'payoneer';
+    const PAYPAL = 'paypal';
+    const PAYPAL_UTOLAG = 'paypal_utolag';
+    const PAYU = 'payu';
+    const PICK_PACK_PONT = 'pick_pack_pont';
+    const POSTAI_CSEKK = 'postai_csekk';
+    const POSTAUTALVANY = 'postautalvany';
+    const SKRILL = 'skrill';
+    const SZEP_CARD = 'szep_card';
+    const TRANSFERWISE = 'transferwise';
+    const UPWORK = 'upwork';
+    const UTALVANY = 'utalvany';
+    const VALTO = 'valto';
+    const WIRE_TRANSFER = 'wire_transfer';
+    const STRIPE = 'stripe';
+
     /**
      * Gets allowable values of the enum
      * @return string[]
@@ -78,32 +81,34 @@ const WIRE_TRANSFER = 'wire_transfer';
     {
         return [
             self::ARUHITEL,
-self::BANKCARD,
-self::BARION,
-self::BARTER,
-self::CASH,
-self::CASH_ON_DELIVERY,
-self::COUPON,
-self::ELORE_UTALAS,
-self::EP_KARTYA,
-self::KOMPENZACIO,
-self::LEVONAS,
-self::ONLINE_BANKCARD,
-self::OTHER,
-self::PAYLIKE,
-self::PAYONEER,
-self::PAYPAL,
-self::PAYPAL_UTOLAG,
-self::PAYU,
-self::PICK_PACK_PONT,
-self::POSTAI_CSEKK,
-self::POSTAUTALVANY,
-self::SKRILL,
-self::SZEP_CARD,
-self::TRANSFERWISE,
-self::UPWORK,
-self::UTALVANY,
-self::VALTO,
-self::WIRE_TRANSFER,        ];
+            self::BANKCARD,
+            self::BARION,
+            self::BARTER,
+            self::CASH,
+            self::CASH_ON_DELIVERY,
+            self::COUPON,
+            self::ELORE_UTALAS,
+            self::EP_KARTYA,
+            self::KOMPENZACIO,
+            self::LEVONAS,
+            self::ONLINE_BANKCARD,
+            self::OTHER,
+            self::PAYLIKE,
+            self::PAYONEER,
+            self::PAYPAL,
+            self::PAYPAL_UTOLAG,
+            self::PAYU,
+            self::PICK_PACK_PONT,
+            self::POSTAI_CSEKK,
+            self::POSTAUTALVANY,
+            self::SKRILL,
+            self::SZEP_CARD,
+            self::TRANSFERWISE,
+            self::UPWORK,
+            self::UTALVANY,
+            self::VALTO,
+            self::WIRE_TRANSFER,
+            self::STRIPE,
+        ];
     }
 }
